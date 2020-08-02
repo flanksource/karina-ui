5cc71badcaf0968043fbf4fac60c170f64a0a9fe
Simba Tsuro <simbathunda@gmail.com>
2020-08-02 11:56:26 +0200

    feat(import data): import data from JSON file
    Data stored in external JSON file
    Bug fix - showing hidden IndicatorIcons
    
    Closes #12

--- src/components/IndicatorCard.vue
+++ src/components/IndicatorCard.vue
@@ -2,11 +2,15 @@
   <v-card flat class="fit-parent px-3">
     <v-row align="center" no-gutters>
       <v-col cols="12" md="2">
-        <img v-bind:src="iconlg" alt="" width=48/>
+     
+
+    <img :src="require(`@/assets/svg/${iconlg}`)" width=48/>
+      
+
       </v-col>
 
       <v-col cols="12" md="10" class="card-bottom">
-        <v-row no-gutters>
+        <v-row>
           <v-col
             v-for="(item, i) in indicatoricons"
             :key="i"
