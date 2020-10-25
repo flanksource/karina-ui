4f4be0ac24480eecb073ac07c09266e55952db1a
simbatsuro <simbaptsuro@gmail.com>
2020-10-25 22:37:39 +0200

    feat: enable dev tools for demo site
    Closes #54

---
+++ src/main-demo.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.config.devtools = true
+Vue.config.productionTip = false
+Vue.prototype.$axios = axios,
+
+
+new Vue({
+
+
+	VueAxios,
+	vuetify,
+	render: h => h(App)
+  
+}).$mount('#app')
