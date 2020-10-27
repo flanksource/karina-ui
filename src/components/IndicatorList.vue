1a3560d863fc74f980c4f70e0c689b8eaaf6d7d5
Simba Tsuro <simbathunda@gmail.com>
2020-10-27 22:24:46 +0200

    feat: add collapsible list

--- src/components/IndicatorList.vue
+++ src/components/IndicatorList.vue
@@ -1,6 +1,5 @@
 <template>
   <span>
-
     <v-expansion-panels
       v-model="list"
       multiple
@@ -9,12 +8,8 @@
       <v-expansion-panel
         v-for="(indicator,i) in indicators"
         :key="i"
-
-        
       >
-   <v-row no-gutters align="">
-
-
+        <v-row no-gutters align="">
           <v-col cols="10" xs="10" sm="10" md="10" lg="10" xl="10" class="pa-0">
             <v-expansion-panels flat class="bordered">
               <v-expansion-panel class="">
@@ -27,39 +22,36 @@
                       width="32"
                     />
                   </v-col>
-
                 </v-expansion-panel-header>
 
-                <v-expansion-panel-content class="pl-9">
-         
-
+                <v-expansion-panel-content class="pl-9 pb-0">
                   <IndicatorCard
                     :indicatoricons="indicator.indicator_icons"
-                  
                   />
-               
                 </v-expansion-panel-content>
               </v-expansion-panel>
             </v-expansion-panels>
           </v-col> 
- 
-            <v-col cols="2" xs="2" sm="2" md="2" lg="1" xl="1" class="px-0">
-          
-             <span class="d-block">{{ getIndicators(indicator.indicator_icons) }}</span>
-
-             <span class="d-block">{{ getIndicators(indicator.indicator_icons) }}</span>
-
-            </v-col>
-           </v-row>
+          <v-col cols="2" xs="2" sm="2" md="2" lg="1" xl="1" class="px-0">
+            <span 
+              class="d-block text-size"
+              v-if="getAlerts(indicator.indicator_icons) > 0"
+            >
+              {{ getAlerts(indicator.indicator_icons) }}</span>
+            <span class="d-block text-size"
+            v-if="getIndicators(indicator.indicator_icons) > 0"
+            >{{ getIndicators(indicator.indicator_icons) }}</span>
+          </v-col>
+        </v-row>
       </v-expansion-panel>
-    </v-expansion-panels> 
+    </v-expansion-panels>
+
     <span>
       <IndicatorCardCanary
         indicatoricon="birdie.svg"
         :canarychecks="canarychecks"
       />
     </span>
-
   </span>
 </template>
 
@@ -104,11 +96,19 @@ export default {
       return count;
     },
     getAlerts(icons) {
-      var count = 0;
+      
+      var sum = 0
       if (icons) {
-        count = icons.length;
+        for (var i=0; i<icons.length; i++) {
+         // console.log(icons[i].color);
+          
+          if (icons[i].color == '#990000' || icons[i].color == '#994c00'){
+            sum = sum + 1;
+            console.log(sum)
+          }
+        }
       }
-      return count;
+       return sum;
     },
   },
 }
@@ -120,45 +120,24 @@ export default {
   .no-events {
     pointer-events: none;
   }
-  .alerts {
-    color:#4a4a4a;
-    float: right;
-    margin-top: 0px;
-    margin-right: 10px;
-  }
-
-  .indicators {
-    color: #4a4a4a;
-    float: right;
- 
-    margin-top: 25px;
-    margin-right: 0px;
-
-  }
-
-  .right-text{
-    border: 2px solid #00f;
-    padding: 0;
-    align-items: unset;
-    display: block;
-    position: unset;
-    min-height: 50px;
-    justify-content: flex-end;
-    
-  }
-
-.v-expansion-panels .v-expansion-panel {
+  .v-expansion-panels .v-expansion-panel {
     margin-top: 0 !important;
-
-
   }
-
-  .bordered {
-    
+  .v-expansion-panel-header {
+     padding: 0 24px 0 24px;
+     min-height: 30px;
   }
-
+  .v-expansion-panel-header:active {
+     padding: 0 24px 0 24px;
+     min-height: 30px;
+  }
+  .v-expansion-panel:active > .v-expansion-panel-header {
+    min-height: 40px;
+  }
+ .v-expansion-panel > .v-expansion-panel-header {
+    min-height: 40px;
+  }
   .bordered::after {
-
     border: 1px dashed #111 ; 
     bottom: 0;
     content: '';
@@ -166,13 +145,9 @@ export default {
     height: 2px;
     left: 20%;
     position: absolute;
-
     width: 73%;
-
   }
-/*for testing*/
-.border{
-  border: 2px solid #00f;
-}
-
+  .text-size {
+    font-size: 12px;
+  }
 </style>
