7c6056d838a596584146a3f91f66942f5bf38851
Simba Tsuro <simbathunda@gmail.com>
2020-10-27 11:44:41 +0200

    feat: add expansion/collapse

--- src/components/IndicatorCard.vue
+++ src/components/IndicatorCard.vue
@@ -1,11 +1,11 @@
 <template>
   <v-row align="center" class="pl-5">
     <v-col cols="2" xs="2" sm="2" md="2" lg="2" xl="2" class="pa-0">
-      <v-img
+     <!--  <v-img
         :aspect-ratio="1 / 1"
         :src="require(`@/assets/svg/${iconlg}`)"
         width="32"
-      />
+      /> -->
     </v-col>
 
     <v-col cols="10" xs="10" sm="8" md="8" lg="8" xl="8" class="pa-0 bordered">
--- src/components/IndicatorList.vue
+++ src/components/IndicatorList.vue
@@ -1,11 +1,62 @@
 <template>
   <span>
-    <span v-for="(indicator, i) in indicators" :key="i">
-      <IndicatorCard
-        :iconlg="indicator.indicator_icon_lg"
-        :indicatoricons="indicator.indicator_icons"
-      />
-    </span>
+
+       <div class="text-center d-flex pb-4">
+      <v-btn @click="expand">
+        expand all
+      </v-btn>
+      <v-btn @click="collapse">
+        collapse all
+      </v-btn>
+    </div>
+
+   <!--  <span v-for="(indicator, i) in indicators" :key="i"> -->
+
+    <v-expansion-panels
+      v-model="list"
+      multiple
+    >
+      <v-expansion-panel
+        v-for="(indicator,i) in indicators"
+        :key="i"
+      >
+        <v-expansion-panel-header>
+          <v-col cols="2" xs="2" sm="2" md="2" lg="2" xl="2" class="pa-0">
+            <v-img
+              :aspect-ratio="1 / 1"
+              :src="require(`@/assets/svg/${indicator.indicator_icon_lg}`)"
+              width="32"
+            />
+          </v-col>
+          <v-col cols="10" xs="10" sm="8" md="8" lg="8" xl="8" class="pa-0 bordered">
+            total
+            total alerts
+
+          </v-col>
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <IndicatorCard
+          
+            :indicatoricons="indicator.indicator_icons"
+          />
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+
+     
+<!--     </span>
+ -->
+
+
+
+
+
+
+
+
+
+
+
 
     <span>
       <IndicatorCardCanary
@@ -13,6 +64,12 @@
         :canarychecks="canarychecks"
       />
     </span>
+
+
+
+
+
+
   </span>
 </template>
 
@@ -33,7 +90,24 @@ export default {
     indicators: Array,
     canarychecks: Array,
   },
-};
+
+  data () {
+    return {
+      list: []
+    }
+  },
+
+  methods: {
+    expand () {
+      this.list = [...this.indicators.keys()].map((k,i) => i)
+    },
+
+    collapse () {
+      this.list = []
+    },
+  },
+}
+
 </script>
 
 
