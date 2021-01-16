7ec7a5be4191790eb6f9836b67f214eee0e3ab15
Philip Stafford Wood <philip.wood@gmail.com>
2021-01-16 22:22:58 +0200

    fixed menu SVGs

--- src/assets/fonts/karina-ui-icons/icons.ts
+++ src/assets/fonts/karina-ui-icons/icons.ts
@@ -1,41 +1,61 @@
 export type IconsId =
+  | "canary-checker"
   | "cargo-ship-50"
   | "cpu"
   | "disk"
+  | "grafana"
+  | "karma"
+  | "kibana"
   | "kubernetes"
   | "linux"
   | "memory"
+  | "prometheus"
   | "triangle-marker"
   | "ubuntu";
 
 export type IconsKey =
+  | "CanaryChecker"
   | "CargoShip_50"
   | "Cpu"
   | "Disk"
+  | "Grafana"
+  | "Karma"
+  | "Kibana"
   | "Kubernetes"
   | "Linux"
   | "Memory"
+  | "Prometheus"
   | "TriangleMarker"
   | "Ubuntu";
 
 export enum Icons {
+  CanaryChecker = "canary-checker",
   CargoShip_50 = "cargo-ship-50",
   Cpu = "cpu",
   Disk = "disk",
+  Grafana = "grafana",
+  Karma = "karma",
+  Kibana = "kibana",
   Kubernetes = "kubernetes",
   Linux = "linux",
   Memory = "memory",
+  Prometheus = "prometheus",
   TriangleMarker = "triangle-marker",
   Ubuntu = "ubuntu",
 }
 
 export const ICONS_CODEPOINTS: { [key in Icons]: string } = {
-  [Icons.CargoShip_50]: "61697",
-  [Icons.Cpu]: "61698",
-  [Icons.Disk]: "61699",
-  [Icons.Kubernetes]: "61700",
-  [Icons.Linux]: "61701",
-  [Icons.Memory]: "61702",
-  [Icons.TriangleMarker]: "61703",
-  [Icons.Ubuntu]: "61704",
+  [Icons.CanaryChecker]: "61697",
+  [Icons.CargoShip_50]: "61698",
+  [Icons.Cpu]: "61699",
+  [Icons.Disk]: "61700",
+  [Icons.Grafana]: "61701",
+  [Icons.Karma]: "61702",
+  [Icons.Kibana]: "61703",
+  [Icons.Kubernetes]: "61704",
+  [Icons.Linux]: "61705",
+  [Icons.Memory]: "61706",
+  [Icons.Prometheus]: "61707",
+  [Icons.TriangleMarker]: "61708",
+  [Icons.Ubuntu]: "61709",
 };
