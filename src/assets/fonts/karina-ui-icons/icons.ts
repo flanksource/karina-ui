bf1f6b1dfb12aecf03eb81a42a6c81d2ee80cafe
Philip Stafford Wood <philip.wood@gmail.com>
2021-01-27 22:51:25 +0200

    add fonts

--- src/assets/fonts/karina-ui-icons/icons.ts
+++ src/assets/fonts/karina-ui-icons/icons.ts
@@ -8,29 +8,101 @@ export type IconsId =
   | "canary-checker"
   | "cancel-50"
   | "cargo-ship-50"
+  | "cd-50"
+  | "checked-50"
   | "cluster"
+  | "commit-git-50-2"
+  | "commit-git-50-3"
+  | "commit-git-50"
   | "commit"
+  | "compare-git-50"
   | "cpu"
+  | "dashboard-50"
   | "disk"
+  | "do-not-disturb-50"
+  | "docker-50-2"
+  | "docker-50-3"
+  | "ethernet-settings-50"
+  | "fires-50-2"
+  | "fires-50"
+  | "geography-50-2"
+  | "geography-50-3"
+  | "geography-50"
+  | "git-50"
   | "git"
+  | "github-50-2"
+  | "github-50-3"
+  | "github-50"
+  | "google-alerts-50"
   | "grafana"
+  | "guarantee-50"
+  | "hdd-50"
+  | "heart"
   | "heartline"
+  | "heavy-rain-50"
+  | "helm-icon-50"
+  | "high-priority-50"
+  | "hub-50"
+  | "idea-50"
   | "idea"
+  | "individual-server-50"
   | "karma"
   | "kibana"
   | "kubernetes"
   | "linux"
   | "memory"
+  | "motherboard-50"
+  | "new-document-50"
   | "node"
+  | "octocat-50-2"
+  | "octocat-50"
+  | "ok-50"
+  | "openconnect-50"
+  | "openid-50"
+  | "process-50"
+  | "processor-50-2"
   | "prometheus"
+  | "pull-request-50-2"
+  | "pull-request-50-3"
+  | "pull-request-50"
+  | "rain-50"
+  | "rainfall-50"
   | "ram"
+  | "router-50"
+  | "safe"
+  | "security-ssl-50"
+  | "security-wi-fi-50"
   | "server"
+  | "ship-wheel-50"
   | "smile"
+  | "snow-50"
+  | "snow-storm-50"
+  | "speech-bubble-50"
+  | "square"
+  | "ssd-50"
   | "storage"
+  | "storm-50"
+  | "stormy-weather-50"
+  | "summer-50-2"
+  | "summer-50"
+  | "tag-window-50"
+  | "threat"
+  | "tick-box-50"
+  | "toolbox-50"
+  | "torrential-rain-50"
   | "triangle-marker"
+  | "tsunami-50"
   | "ubuntu"
+  | "unavailable-50"
+  | "vip-50"
   | "vmware-50"
-  | "webhook-50";
+  | "webhook-50"
+  | "wet-50"
+  | "wind-50"
+  | "windy-weather-50"
+  | "winter-50-2"
+  | "winter-50"
+  | "workspace-50";
 
 export type IconsKey =
   | "About_50"
@@ -42,29 +114,101 @@ export type IconsKey =
   | "CanaryChecker"
   | "Cancel_50"
   | "CargoShip_50"
+  | "Cd_50"
+  | "Checked_50"
   | "Cluster"
+  | "CommitGit_50_2"
+  | "CommitGit_50_3"
+  | "CommitGit_50"
   | "Commit"
+  | "CompareGit_50"
   | "Cpu"
+  | "Dashboard_50"
   | "Disk"
+  | "DoNotDisturb_50"
+  | "Docker_50_2"
+  | "Docker_50_3"
+  | "EthernetSettings_50"
+  | "Fires_50_2"
+  | "Fires_50"
+  | "Geography_50_2"
+  | "Geography_50_3"
+  | "Geography_50"
+  | "Git_50"
   | "Git"
+  | "Github_50_2"
+  | "Github_50_3"
+  | "Github_50"
+  | "GoogleAlerts_50"
   | "Grafana"
+  | "Guarantee_50"
+  | "Hdd_50"
+  | "Heart"
   | "Heartline"
+  | "HeavyRain_50"
+  | "HelmIcon_50"
+  | "HighPriority_50"
+  | "Hub_50"
+  | "Idea_50"
   | "Idea"
+  | "IndividualServer_50"
   | "Karma"
   | "Kibana"
   | "Kubernetes"
   | "Linux"
   | "Memory"
+  | "Motherboard_50"
+  | "NewDocument_50"
   | "Node"
+  | "Octocat_50_2"
+  | "Octocat_50"
+  | "Ok_50"
+  | "Openconnect_50"
+  | "Openid_50"
+  | "Process_50"
+  | "Processor_50_2"
   | "Prometheus"
+  | "PullRequest_50_2"
+  | "PullRequest_50_3"
+  | "PullRequest_50"
+  | "Rain_50"
+  | "Rainfall_50"
   | "Ram"
+  | "Router_50"
+  | "Safe"
+  | "SecuritySsl_50"
+  | "SecurityWiFi_50"
   | "Server"
+  | "ShipWheel_50"
   | "Smile"
+  | "Snow_50"
+  | "SnowStorm_50"
+  | "SpeechBubble_50"
+  | "Square"
+  | "Ssd_50"
   | "Storage"
+  | "Storm_50"
+  | "StormyWeather_50"
+  | "Summer_50_2"
+  | "Summer_50"
+  | "TagWindow_50"
+  | "Threat"
+  | "TickBox_50"
+  | "Toolbox_50"
+  | "TorrentialRain_50"
   | "TriangleMarker"
+  | "Tsunami_50"
   | "Ubuntu"
+  | "Unavailable_50"
+  | "Vip_50"
   | "Vmware_50"
-  | "Webhook_50";
+  | "Webhook_50"
+  | "Wet_50"
+  | "Wind_50"
+  | "WindyWeather_50"
+  | "Winter_50_2"
+  | "Winter_50"
+  | "Workspace_50";
 
 export enum Icons {
   About_50 = "about-50",
@@ -76,29 +220,101 @@ export enum Icons {
   CanaryChecker = "canary-checker",
   Cancel_50 = "cancel-50",
   CargoShip_50 = "cargo-ship-50",
+  Cd_50 = "cd-50",
+  Checked_50 = "checked-50",
   Cluster = "cluster",
+  CommitGit_50_2 = "commit-git-50-2",
+  CommitGit_50_3 = "commit-git-50-3",
+  CommitGit_50 = "commit-git-50",
   Commit = "commit",
+  CompareGit_50 = "compare-git-50",
   Cpu = "cpu",
+  Dashboard_50 = "dashboard-50",
   Disk = "disk",
+  DoNotDisturb_50 = "do-not-disturb-50",
+  Docker_50_2 = "docker-50-2",
+  Docker_50_3 = "docker-50-3",
+  EthernetSettings_50 = "ethernet-settings-50",
+  Fires_50_2 = "fires-50-2",
+  Fires_50 = "fires-50",
+  Geography_50_2 = "geography-50-2",
+  Geography_50_3 = "geography-50-3",
+  Geography_50 = "geography-50",
+  Git_50 = "git-50",
   Git = "git",
+  Github_50_2 = "github-50-2",
+  Github_50_3 = "github-50-3",
+  Github_50 = "github-50",
+  GoogleAlerts_50 = "google-alerts-50",
   Grafana = "grafana",
+  Guarantee_50 = "guarantee-50",
+  Hdd_50 = "hdd-50",
+  Heart = "heart",
   Heartline = "heartline",
+  HeavyRain_50 = "heavy-rain-50",
+  HelmIcon_50 = "helm-icon-50",
+  HighPriority_50 = "high-priority-50",
+  Hub_50 = "hub-50",
+  Idea_50 = "idea-50",
   Idea = "idea",
+  IndividualServer_50 = "individual-server-50",
   Karma = "karma",
   Kibana = "kibana",
   Kubernetes = "kubernetes",
   Linux = "linux",
   Memory = "memory",
+  Motherboard_50 = "motherboard-50",
+  NewDocument_50 = "new-document-50",
   Node = "node",
+  Octocat_50_2 = "octocat-50-2",
+  Octocat_50 = "octocat-50",
+  Ok_50 = "ok-50",
+  Openconnect_50 = "openconnect-50",
+  Openid_50 = "openid-50",
+  Process_50 = "process-50",
+  Processor_50_2 = "processor-50-2",
   Prometheus = "prometheus",
+  PullRequest_50_2 = "pull-request-50-2",
+  PullRequest_50_3 = "pull-request-50-3",
+  PullRequest_50 = "pull-request-50",
+  Rain_50 = "rain-50",
+  Rainfall_50 = "rainfall-50",
   Ram = "ram",
+  Router_50 = "router-50",
+  Safe = "safe",
+  SecuritySsl_50 = "security-ssl-50",
+  SecurityWiFi_50 = "security-wi-fi-50",
   Server = "server",
+  ShipWheel_50 = "ship-wheel-50",
   Smile = "smile",
+  Snow_50 = "snow-50",
+  SnowStorm_50 = "snow-storm-50",
+  SpeechBubble_50 = "speech-bubble-50",
+  Square = "square",
+  Ssd_50 = "ssd-50",
   Storage = "storage",
+  Storm_50 = "storm-50",
+  StormyWeather_50 = "stormy-weather-50",
+  Summer_50_2 = "summer-50-2",
+  Summer_50 = "summer-50",
+  TagWindow_50 = "tag-window-50",
+  Threat = "threat",
+  TickBox_50 = "tick-box-50",
+  Toolbox_50 = "toolbox-50",
+  TorrentialRain_50 = "torrential-rain-50",
   TriangleMarker = "triangle-marker",
+  Tsunami_50 = "tsunami-50",
   Ubuntu = "ubuntu",
+  Unavailable_50 = "unavailable-50",
+  Vip_50 = "vip-50",
   Vmware_50 = "vmware-50",
   Webhook_50 = "webhook-50",
+  Wet_50 = "wet-50",
+  Wind_50 = "wind-50",
+  WindyWeather_50 = "windy-weather-50",
+  Winter_50_2 = "winter-50-2",
+  Winter_50 = "winter-50",
+  Workspace_50 = "workspace-50",
 }
 
 export const ICONS_CODEPOINTS: { [key in Icons]: string } = {
@@ -111,27 +327,99 @@ export const ICONS_CODEPOINTS: { [key in Icons]: string } = {
   [Icons.CanaryChecker]: "61703",
   [Icons.Cancel_50]: "61704",
   [Icons.CargoShip_50]: "61705",
-  [Icons.Cluster]: "61706",
-  [Icons.Commit]: "61707",
-  [Icons.Cpu]: "61708",
-  [Icons.Disk]: "61709",
-  [Icons.Git]: "61710",
-  [Icons.Grafana]: "61711",
-  [Icons.Heartline]: "61712",
-  [Icons.Idea]: "61713",
-  [Icons.Karma]: "61714",
-  [Icons.Kibana]: "61715",
-  [Icons.Kubernetes]: "61716",
-  [Icons.Linux]: "61717",
-  [Icons.Memory]: "61718",
-  [Icons.Node]: "61719",
-  [Icons.Prometheus]: "61720",
-  [Icons.Ram]: "61721",
-  [Icons.Server]: "61722",
-  [Icons.Smile]: "61723",
-  [Icons.Storage]: "61724",
-  [Icons.TriangleMarker]: "61725",
-  [Icons.Ubuntu]: "61726",
-  [Icons.Vmware_50]: "61727",
-  [Icons.Webhook_50]: "61728",
+  [Icons.Cd_50]: "61706",
+  [Icons.Checked_50]: "61707",
+  [Icons.Cluster]: "61708",
+  [Icons.CommitGit_50_2]: "61709",
+  [Icons.CommitGit_50_3]: "61710",
+  [Icons.CommitGit_50]: "61711",
+  [Icons.Commit]: "61712",
+  [Icons.CompareGit_50]: "61713",
+  [Icons.Cpu]: "61714",
+  [Icons.Dashboard_50]: "61715",
+  [Icons.Disk]: "61716",
+  [Icons.DoNotDisturb_50]: "61717",
+  [Icons.Docker_50_2]: "61718",
+  [Icons.Docker_50_3]: "61719",
+  [Icons.EthernetSettings_50]: "61720",
+  [Icons.Fires_50_2]: "61721",
+  [Icons.Fires_50]: "61722",
+  [Icons.Geography_50_2]: "61723",
+  [Icons.Geography_50_3]: "61724",
+  [Icons.Geography_50]: "61725",
+  [Icons.Git_50]: "61726",
+  [Icons.Git]: "61727",
+  [Icons.Github_50_2]: "61728",
+  [Icons.Github_50_3]: "61729",
+  [Icons.Github_50]: "61730",
+  [Icons.GoogleAlerts_50]: "61731",
+  [Icons.Grafana]: "61732",
+  [Icons.Guarantee_50]: "61733",
+  [Icons.Hdd_50]: "61734",
+  [Icons.Heart]: "61735",
+  [Icons.Heartline]: "61736",
+  [Icons.HeavyRain_50]: "61737",
+  [Icons.HelmIcon_50]: "61738",
+  [Icons.HighPriority_50]: "61739",
+  [Icons.Hub_50]: "61740",
+  [Icons.Idea_50]: "61741",
+  [Icons.Idea]: "61742",
+  [Icons.IndividualServer_50]: "61743",
+  [Icons.Karma]: "61744",
+  [Icons.Kibana]: "61745",
+  [Icons.Kubernetes]: "61746",
+  [Icons.Linux]: "61747",
+  [Icons.Memory]: "61748",
+  [Icons.Motherboard_50]: "61749",
+  [Icons.NewDocument_50]: "61750",
+  [Icons.Node]: "61751",
+  [Icons.Octocat_50_2]: "61752",
+  [Icons.Octocat_50]: "61753",
+  [Icons.Ok_50]: "61754",
+  [Icons.Openconnect_50]: "61755",
+  [Icons.Openid_50]: "61756",
+  [Icons.Process_50]: "61757",
+  [Icons.Processor_50_2]: "61758",
+  [Icons.Prometheus]: "61759",
+  [Icons.PullRequest_50_2]: "61760",
+  [Icons.PullRequest_50_3]: "61761",
+  [Icons.PullRequest_50]: "61762",
+  [Icons.Rain_50]: "61763",
+  [Icons.Rainfall_50]: "61764",
+  [Icons.Ram]: "61765",
+  [Icons.Router_50]: "61766",
+  [Icons.Safe]: "61767",
+  [Icons.SecuritySsl_50]: "61768",
+  [Icons.SecurityWiFi_50]: "61769",
+  [Icons.Server]: "61770",
+  [Icons.ShipWheel_50]: "61771",
+  [Icons.Smile]: "61772",
+  [Icons.Snow_50]: "61773",
+  [Icons.SnowStorm_50]: "61774",
+  [Icons.SpeechBubble_50]: "61775",
+  [Icons.Square]: "61776",
+  [Icons.Ssd_50]: "61777",
+  [Icons.Storage]: "61778",
+  [Icons.Storm_50]: "61779",
+  [Icons.StormyWeather_50]: "61780",
+  [Icons.Summer_50_2]: "61781",
+  [Icons.Summer_50]: "61782",
+  [Icons.TagWindow_50]: "61783",
+  [Icons.Threat]: "61784",
+  [Icons.TickBox_50]: "61785",
+  [Icons.Toolbox_50]: "61786",
+  [Icons.TorrentialRain_50]: "61787",
+  [Icons.TriangleMarker]: "61788",
+  [Icons.Tsunami_50]: "61789",
+  [Icons.Ubuntu]: "61790",
+  [Icons.Unavailable_50]: "61791",
+  [Icons.Vip_50]: "61792",
+  [Icons.Vmware_50]: "61793",
+  [Icons.Webhook_50]: "61794",
+  [Icons.Wet_50]: "61795",
+  [Icons.Wind_50]: "61796",
+  [Icons.WindyWeather_50]: "61797",
+  [Icons.Winter_50_2]: "61798",
+  [Icons.Winter_50]: "61799",
+  [Icons.Workspace_50]: "61800",
 };
